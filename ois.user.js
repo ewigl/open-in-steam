5f0be484fd7cc0ee205e058cc04531df7a78c58c
ewigl <ewiglichtl@gmail.com>
2024-09-08 13:58:57 +0800

    add @noframes to prevent iframe issue

--- ois.user.js
+++ ois.user.js
@@ -2,7 +2,7 @@
 // @name         在 Steam(客户端) 中打开 - Open in Steam
 // @name:en-US   Open (Web Page) in Steam (Client)
 // @namespace    https://github.com/ewigl/open-in-steam
-// @version      0.3.2
+// @version      0.3.3
 // @description  在 Steam 网页中添加一个按钮, 以快速在 Steam(客户端) 中打开当前页面
 // @description:en-US  Add a button to open the current page in Steam (Client)
 // @author       Licht
@@ -10,6 +10,7 @@
 // @homepage     https://github.com/ewigl/open-in-steam
 // @match        *://*.steampowered.com/*
 // @match        *://*.steamcommunity.com/*
+// @noframes
 // @icon         https://store.steampowered.com/favicon.ico
 // @grant        GM_addStyle
 // ==/UserScript==
